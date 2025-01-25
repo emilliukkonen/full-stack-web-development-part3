35c72789fe822bb5c5bcab10854dee9620b31182
emilliukkonen <emil.liukkonen@gmail.com>
2025-01-25 18:39:40 +0200

    exercises 3.7-3.8

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
+
+morgan.token('body', (req) => {return req.method === 'POST' ? JSON.stringify(req.body) : ''})
 
 app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
     { 
