e43f7e231b16c392d2a4810f48f27b9776c1d760
emilliukkonen <emil.liukkonen@gmail.com>
2025-01-25 21:41:30 +0200

    adding frontend through dist

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ morgan.token('body', (req) => {return req.method === 'POST' ? JSON.stringify(req
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(cors())
+app.use(express.static('dist'))
 
 let persons = [
     { 
