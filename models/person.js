97204cc7c35e8a6b70bb3bd2484f4ab38f3f4606
emilliukkonen <emil.liukkonen@gmail.com>
2025-01-27 19:31:38 +0200

    excercises until 3.21

--- models/person.js
+++ models/person.js
@@ -16,9 +16,23 @@ mongoose.connect(url)
     })
 
 const PersonSchema = new mongoose.Schema({
-    name: String,
-    number: String,
-    })
+    name: {
+      type: String,
+      minLength: 3,
+      required: true
+    },
+    number: {
+      type: String,
+      minLength: 8,
+      validate: {
+        validator: function(v) {
+          return /^\d{2,3}-\d+$/.test(v);
+        },
+        message: props => `${props.value} is not a valid phone number!`
+      },
+      required: true
+    }
+})
 
 PersonSchema.set('toJSON', {
   transform: (document, returnedObject) => {
